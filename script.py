2d878071bcbbde307bfdbd1462af00200204e327
mestromezer <94231152+mestromezer@users.noreply.github.com>
2022-11-05 14:55:01 +0400

    Update script.py

--- script.py
+++ script.py
@@ -16,7 +16,6 @@ if __name__=="__main__":
     
     str = input("Input legs(sep by space): ")
     str_args = str.split(' ')
-    str_args = [x for x in str_args if float(x)]
     
     for i in range(0,2): str_args[i] = float(str_args[i])
     
